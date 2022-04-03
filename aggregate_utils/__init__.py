88fba13af8633e9d7469b7f61a4c3f56c3f2ee52
csecht <spam-me@earthlink.net>
2022-04-03 08:16:26 -0500

    ver 0.4.25: restructure authorship info; optimize imports

--- aggregate_utils/__init__.py
+++ aggregate_utils/__init__.py
@@ -1,15 +1,22 @@
-import sys
-from os import system
+#!/usr/bin/env python3
+"""
+Initialize module information, startup dictionaries and housekeeping.
+"""
+import os
 
-from aggregate_utils import vcheck, get_user_agent, config as cfg
+from . import get_user_agent, vcheck, config as cfg
+
+__author__ = 'Craig S. Echt'
+__copyright__ = 'Copyright (C) 2022 C.S. Echt'
+__version__ = '0.4.25'
 
 about = {
-    'author': 'Craig Echt',
-    'copyright': 'Copyright (C) 2022 C.S. Echt',
+    'author': __author__,
+    'copyright': __copyright__,
     'license': 'GNU General Public License',
     'program_name': 'aggregate_search.py',
     'project_url': 'https://github.com/csecht/search-aggregator',
-    'version': '0.4.25',
+    'version': __version__,
     'credits': 'Tasos M Adamopoulos (tasos-py) and Mario Vilas',
     'dev_environment': 'Python 3.8',
     'status': 'Development Status :: 1 - Alpha',
@@ -27,5 +34,5 @@ agent = {
 vcheck.minversion('3.6')
 
 # Needed for Windows Command Prompt ANSI text formatting.
-if sys.platform[:3] == 'win':
-    system("color")
+if os.name == 'nt':
+    os.system("color")
