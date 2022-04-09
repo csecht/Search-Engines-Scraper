72b2e7b01b58b49e1f33e3498cc25604ea64d717
csecht <spam-me@earthlink.net>
2022-04-09 10:07:20 -0500

    ver 0.5.9: remove bot UA options from Mojeek searches

--- aggregate_utils/config.py
+++ aggregate_utils/config.py
@@ -16,12 +16,12 @@ ENGINE_NAMES = {
 # Assign a random user agent function from get_user_agents() to each engine.
 # Any agent may be temporarily blocked when too frequent requests are made,
 #   so try to maximize the choice of most likely to be accepted agents.
-# Bot agents, 'bua', are generally banned by SP; tua may be blocked by Moj.
+# Bot agents, 'bua', are generally banned by SP & Moj; 'gua' sometimes by Moj.
 # These tuples of dictionary keys are a mix of static and random user agent
 #   functions for random selection by get_user_agents.rando_function().
 DDG_UAs = ('bua', 'fua', 'gua')
 MG_UAs = ('bua', 'fua', 'pua', 'gua', 'rua', 'rua')
-MOJ_UAs = ('bua', 'fua', 'gua', 'rua', 'rua')
+MOJ_UAs = ('fua', 'gua', 'rua', 'rua')
 SP_UAs = ('fua', 'wua')
 
 # Terminal output ANSI foreground colors.
